b563612743612c03d8c659f0b9209cf138bb54b8
Brendan <brendanekane@gmail.com>
2018-07-17 21:06:34 -0400

    required vendor in entry file

--- lib/little_light.rb
+++ lib/little_light.rb
@@ -2,6 +2,7 @@ require "little_light/version"
 require 'little_light/client'
 require 'little_light/user'
 require 'little_light/clan'
+require 'little_light/vendor'
 
 module LittleLight
   def self.new(options={})
